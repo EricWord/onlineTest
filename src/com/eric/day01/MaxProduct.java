203dfdb32dd4798ca34056ec370d72b2b8dd9b80
tom_gl <xiaocui@163.com>
2019-03-09 13:39:04 +0800

    Initial commit

---
+++ src/com/eric/day01/MaxProduct.java
@@ -0,0 +1,11 @@
+package com.eric.day01;
+
+/**
+ * @ClassName: MaxProduct
+ * @Description: 给定一个无序数组，包含正数、负数和0，要求从中找出3个数的乘积，使得乘积最大，要求时间复杂度：O(n)，空间复杂度：O(1)
+ * @Author: Eric
+ * @Date: 2019/3/9 0009
+ * @Email: xiao_cui_vip@163.com
+ */
+public class MaxProduct {
+}
